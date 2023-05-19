9598d7c5fe745165929036d03143382721dace83
Your Name <you@example.com>
2023-05-19 17:31:44 +0630

    update form design in adminstrator page

--- adminstrator/scriptForAdmin.js
+++ adminstrator/scriptForAdmin.js
@@ -48,33 +48,35 @@ function createData() {
   var gender = $("#gender").val();
   var phone = $("#phone").val();
   var address = $("#address").val();
-  var classOption = $("#class").val();
+  var classOption = $("input[name='class[]']:checked").map(function() {
+    return $(this).val();
+  }).get();
 
-  // Form validation
-  if (name === '' || !(isNaN(name))) {
-    alert("Name is required and should not include characters");
-    return;
-  }
+ // form validator
+if (name === '' || !(isNaN(name))) {
+  alert("Name is required and should not include characters");
+  return;
+}
 
-  if (isNaN(age) || age < 5) {
-    alert("Invalid age. Age must be a number and greater than 5");
-    return;
-  }
+if (isNaN(age) || age < 5) {
+  alert("Invalid age. Age must be a number and greater than 5");
+  return;
+}
 
-  if (gender === '') {
-    alert("Gender is required");
-    return;
-  }
+if (gender === '') {
+  alert("Gender is required");
+  return;
+}
 
-  if (isNaN(phone) || phone.length < 9) {
-    alert("Phone number must contain only numbers and the length should be 9 digits");
-    return;
-  }
+if (isNaN(phone) || phone.length < 9) {
+  alert("Phone number must contain only numbers and the length should be 9 digits");
+  return;
+}
 
-  if (classOption === '') {
-    alert("Class option is required");
-    return;
-  }
+if (classOption === null||classOption.length === 0) {
+  alert("Please select at least one class");
+  return;
+}
 
   // Create the form data object
   var formData = {
